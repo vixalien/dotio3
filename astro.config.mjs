1dde82028e597ed0d4f9c5ec377d0f2d2358c899
Angelo Verlain <hey@vixalien.com>
2025-07-11 14:57:55 +0200

    move to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,19 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.vixalien.com",
-	integrations: [sitemap()],
-	redirects: {
-		"/posts": "/",
-		"/feed": "/rss.xml",
+  site: "https://www.vixalien.com",
+  integrations: [sitemap()],
+
+  redirects: {
+      "/posts": "/",
+      "/feed": "/rss.xml",
 	},
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
