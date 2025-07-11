a97124559d1c896d2d9c250a56f7172854298934
Angelo Verlain <hey@vixalien.com>
2025-07-11 15:09:10 +0200

    some stylstic changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "vixalien´s site";
+export const SITE_TITLE = "vixalien's site";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const AUTHOR = "vixalien";
 export const PUBLISH_YEAR = new Date().getFullYear();
